e2e08bd3adf35dc035e7562cf32d1ead2044d8c6
Gintaras <99560038+Gintaras68@users.noreply.github.com>
2026-02-17 11:24:16 +0200

    create "social-metrics" basic

--- js/components/socialMetrics.js
+++ js/components/socialMetrics.js
@@ -2,8 +2,22 @@ function socialMetrics(selector, data) {
   if (typeof selector !== "string" || selector === "") {
     return false;
   }
-  
-  console.log("SOCIAL METRICS ...");
+
+  if (!Array.isArray(data) || data.length === 0) {
+    return false;
+  }
+  const DOM = document.getElementById(selector);
+  let HTML = "";
+
+  for (const item of data) {
+    HTML += ` <li class="metric">
+                <div class="number">${item.number + item.symbol}</div>
+                <div class="label">${item.label}</div>
+              </li>`;
+  }
+
+  DOM.innerHTML = HTML;
+  DOM.classList.add("social-metrics");
 }
 
 export { socialMetrics };
