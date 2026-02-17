c037111413bbbc021f17fa2531d91dcf491a6d2d
Gintaras <99560038+Gintaras68@users.noreply.github.com>
2026-02-17 10:44:12 +0200

    start work with socialMetrics

---
+++ js/components/socialMetrics.js
@@ -0,0 +1,9 @@
+function socialMetrics(selector, data) {
+  if (typeof selector !== "string" || selector === "") {
+    return false;
+  }
+  
+  console.log("SOCIAL METRICS ...");
+}
+
+export { socialMetrics };
